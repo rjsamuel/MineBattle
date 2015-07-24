f152d65756d2082faa9ba92e85f69896d4538b0d
Remo Samuel <rjsamuel@hotmail.co.uk>
2015-07-24 22:33:13 +0100

    Player class
    Added variables and functions to the player class

--- minebattle/src/main/Player.java
+++ minebattle/src/main/Player.java
@@ -1,5 +1,29 @@
 package main;
 
 public class Player {
-
+	
+	private String displayName;
+	private int kills;
+	private int deaths;
+	//private DateTime timeAlive;
+	
+	public String getDisplayName()
+	{
+		return displayName;
+	}
+	
+	public int getKills()
+	{
+		return kills;
+	}
+	
+	public int getDeaths()
+	{
+		return deaths;
+	}
+	
+	public double getKdRatio()
+	{
+		return kills/deaths;
+	}
 }
